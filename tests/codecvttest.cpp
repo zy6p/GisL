c97f8bbb6b9c705809398ba461c31b8c212feefc
lusir <goooog@88.com>
2021-03-08 10:28:31 +0800

    update testdata

--- tests/codecvttest.cpp
+++ tests/codecvttest.cpp
@@ -7,24 +7,24 @@
 
 int main(int argc, char *argv[]) {
     GisL::DaEncoder encoder;
-    encoder.loadTextFile2Text( "../resource/practise_1/Decode2.txt" );
+    encoder.loadTextFile2Text( "data/practise_1/Decode2.txt" );
     encoder.encode();
-    encoder.writeBinaryFile( "../resource/practise_1/Ddddecode2.da" );
+    encoder.writeBinaryFile( "data/practise_1/Ddddecode2.da" );
 //    encoder.clear();
     GisL::DaDecoder decoder;
-    decoder.loadBinaryFile( "../resource/practise_1/Ddddecode2.da" );
+    decoder.loadBinaryFile( "data/practise_1/Ddddecode2.da" );
     decoder.decode();
-    decoder.writeTextFile( "../resource/practise_1/BDsdDecode2.txt" );
+    decoder.writeTextFile( "data/practise_1/BDsdDecode2.txt" );
     return 1;
 //    GisL::DaEncoder *pEncoder = new GisL::DaEncoder();
-//    pEncoder->loadTextFile2Text("../resource/practise_1/Decode2.txt");
+//    pEncoder->loadTextFile2Text("data/practise_1/Decode2.txt");
 //    pEncoder->encode();
-//    pEncoder->writeBinaryFile("../resource/practise_1/Ddddecode2.da");
+//    pEncoder->writeBinaryFile("data/practise_1/Ddddecode2.da");
 //    delete pEncoder;
 //    GisL::DaDecoder *pDecoder = new GisL::DaDecoder();
-//    pDecoder->loadBinaryFile("../resource/practise_1/Ddddecode2.da");
+//    pDecoder->loadBinaryFile("data/practise_1/Ddddecode2.da");
 //    pDecoder->decode();
-//    pDecoder->writeTextFile("../resource/practise_1/BDsdDecode2.txt");
+//    pDecoder->writeTextFile("data/practise_1/BDsdDecode2.txt");
 //    delete pDecoder;
 //    return 1;
 }
