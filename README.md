57837fbee90f9ec709a822feeee6dfc8c184bf85
knowmore <goooog@88.com>
2021-06-12 00:14:25 +0800

    fix: cmake raster operate

--- README.md
+++ README.md
@@ -2,9 +2,43 @@
 
 This program is a gis software -- practice for 3d Visualization lecture.
 
-Official website is [gisl](https://www.hotdry.top/gisl/). It can encode and decode text. It can open and write .shp, .dbf, and .geojson. It also can use .sld to render layer style. And it can convert dem to mesh/tin and read tin/mesh data. In a word, it is a easy-to-use software.
+Official website is [gisl](https://www.hotdry.top/gisl/). It can encode and decode text. It can open and write .shp,
+.dbf, and .geojson. It also can use .sld to render layer style. And it can convert dem to mesh/tin and read tin/mesh
+data. In a word, it is a easy-to-use software.
 
 ![image](https://user-images.githubusercontent.com/55835958/116175480-f67d0280-a742-11eb-987f-7db72c9d9d78.png)
+
+## Build
+
+1. git clone
+
+```shell
+git clone https://github.com/zy6p/GisL.git
+cd GisL
+git submodule update --init --recursive
+```
+
+2. install depth(ubuntu 20.04)
+
+```shell
+sudo su
+./scripts/ubuntuGiser.sh
+cd extern/abseil-cpp
+mkdir build
+cmake -B build -S .
+cmake --build build --target install
+cd ../gmt
+cmake -B build -S .
+cmake --build build --target install
+cd ../..
+```
+
+3. build
+
+```shell
+cmake -B build -S .
+cmake --build build --target install
+```
 
 ## Feature
 
--- scripts/ubuntuGiser.sh
+++ scripts/ubuntuGiser.sh
@@ -1,50 +1,6 @@
-sudo apt-get install gcc cmake build-essential qt-creator qt5-default libgdal-dev gdal-bin python python3 python-pip python3-pip ccache git g++
-sudo apt-get install ubuntu-desktop
-sudo apt install -y virtualenv curl python-setuptools build-essential gcc g++ cmake python3-dev python-virtualenv binutils libproj-dev 
-sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
-sudo apt-get update
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
+apt-get install ubuntu-desktop gcc cmake build-essential qtcreator qt5-default libgdal-dev gdal-bin python python3 python-pip python3-pip ccache git g++virtualenv curl python-setuptools build-essential gcc g++ cmake python3-dev binutils libproj-dev libgdal-dev python3-gdal gdal-bin doxygen-latex  doxygen-doc doxygen-gui graphviz qgisqttools5* qtpositioning5-dev gmt
+add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+apt-get update && apt-get upgrade -y
+#update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+#update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
 python -m pip install sphinx breathe
-# 首先更新apt源
-sudo apt-get update && sudo apt-get upgrade -y
-# 安装 PROJ
-sudo apt-get install libproj-dev proj-data proj-bin -y
-# 安装 GEOS
-sudo apt-get install libgeos-dev -y
-# 安装 GDAL
-sudo apt-get install libgdal-dev python3-gdal gdal-bin -y
-# 安装 PDAL 
-sudo apt-get install libpdal-dev pdal libpdal-plugin-python -y
-sudo apt-get install doxygen-latex  doxygen-doc doxygen-gui graphviz
-sudo apt-get install qgis qgis-server qgis-plugin-grass
-
-sudo apt install libgl1-mesa-dev libglu1-mesa-dev freeglut3 freeglut3-dev nginx
-
-sudo apt-get install unar
-sudo apt-get install flex bison
-sudo apt install libzip-dev
-sudo apt-get install autoconf automake libtool curl make g++ unzip
-
-sudo apt-get install libprotobuf* protobuf-c-compiler protobuf-compiler
-sudo apt-get install libqt5serialport5-dev
-
-sudo apt-get install grass grass-core grass-dev grass-gui libgdal-grass qgis-plugin-grass qgis-provider-grass
-sudo apt-get install libqt5*
-sudo apt-get install qtwayland5 -y
-sudo apt-get install qttools5*
-sudo apt install qtpositioning5-dev
-sudo apt install -y qca* libqca* libgsl*
-
-python -m pip install --upgrade pip
-#python -m pip install pyqt5 pyqt5-tools sip
-sudo apt install sip-dev python3-pyqt5 python-pyqt5 python-pyqt5-sip
-
-sudo apt install exiv2 libexiv2*
-
-sudo apt install libssl-dev sqlite3 libsqlite3-dev libqt5webkit5-dev qttools5-dev qttools5-dev-tools python-sphinx texlive-latex-base inotify-tools qt5keychain-dev
-sudo apt install libqwt-qt5-dev libqwt-dev
-sudo apt install libqsci*-qt5*
-
-sudo apt install libspatialindex*
-python -m pip install qscintilla
