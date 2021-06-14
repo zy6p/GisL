11262057740f4727a0ecf18b0f06444ac1e26065
Restyled.io <commits@restyled.io>
2021-06-14 05:23:04 +0000

    Restyled by clang-format

--- src/core/provider/rasterprovider.h
+++ src/core/provider/rasterprovider.h
@@ -18,22 +18,22 @@ namespace gisl {
  */
 class RasterProvider final : public DataProvider {
 public:
-    RasterProvider();
-    void loadData(const std::string& theFileName) override;
-    void combinePrint(int band1, int band2, int band3) override;
-    void
-    combinePrint(int band1, int band2, int band3, PainterFactory& p) override;
-    [[nodiscard]] const std::vector<std::shared_ptr<RasterBand>>&
-            getPmBand() const;
-    void setContrastEnhancementMethod(
-        RasterBand::ContrastEnhancementMethod contrastEnhancementMethod);
+  RasterProvider();
+  void loadData(const std::string& theFileName) override;
+  void combinePrint(int band1, int band2, int band3) override;
+  void
+  combinePrint(int band1, int band2, int band3, PainterFactory& p) override;
+  [[nodiscard]] const std::vector<std::shared_ptr<RasterBand>>&
+  getPmBand() const;
+  void setContrastEnhancementMethod(
+      RasterBand::ContrastEnhancementMethod contrastEnhancementMethod);
 
 private:
-    std::vector<std::shared_ptr<RasterBand>> pmBand;
-    RasterBand::ContrastEnhancementMethod contrastEnhancementMethod =
-        RasterBand::ContrastEnhancementMethod::StretchToCumulative96RealMinMax;
-    int xSize = 0;
-    int ySize = 0;
+  std::vector<std::shared_ptr<RasterBand>> pmBand;
+  RasterBand::ContrastEnhancementMethod contrastEnhancementMethod =
+      RasterBand::ContrastEnhancementMethod::StretchToCumulative96RealMinMax;
+  int xSize = 0;
+  int ySize = 0;
 };
 
 } // namespace gisl
