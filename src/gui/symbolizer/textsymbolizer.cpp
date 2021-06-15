5b880c9a965bc4cfac2eb93a1b9f91aceddce634
Restyled.io <commits@restyled.io>
2021-06-15 03:57:47 +0000

    Restyled by astyle

--- src/gui/symbolizer/textsymbolizer.cpp
+++ src/gui/symbolizer/textsymbolizer.cpp
@@ -9,95 +9,95 @@
 
 namespace gisl {
 void TextSymbolizer::rand() {
-  propertyName = "fid";
+    propertyName = "fid";
 
-  fontFamily = "Times New Roman";
-  fontSize = 13;
+    fontFamily = "Times New Roman";
+    fontSize = 13;
 
-  anchorPointX = 0;
-  anchorPointY = 0;
+    anchorPointX = 0;
+    anchorPointY = 0;
 
-  fillColor.setNamedColor("#000000");
+    fillColor.setNamedColor("#000000");
 
-  maxDisplacement = 1;
+    maxDisplacement = 1;
 }
 
 void TextSymbolizer::init(QXmlStreamReader& sldStream) {
-  sldStream.readNextStartElement();
-  sldStream.readNextStartElement();
-  propertyName = sldStream.readElementText().toStdString();
+    sldStream.readNextStartElement();
+    sldStream.readNextStartElement();
+    propertyName = sldStream.readElementText().toStdString();
 
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNextStartElement();
-  fontFamily = sldStream.readElementText().toStdString();
-  sldStream.readNextStartElement();
-  fontSize = sldStream.readElementText().toInt();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNextStartElement();
+    fontFamily = sldStream.readElementText().toStdString();
+    sldStream.readNextStartElement();
+    fontSize = sldStream.readElementText().toInt();
 
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNextStartElement();
-  sldStream.readNextStartElement();
-  sldStream.readNextStartElement();
-  anchorPointX = sldStream.readElementText().toFloat();
-  sldStream.readNextStartElement();
-  anchorPointY = sldStream.readElementText().toFloat();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNextStartElement();
+    sldStream.readNextStartElement();
+    sldStream.readNextStartElement();
+    anchorPointX = sldStream.readElementText().toFloat();
+    sldStream.readNextStartElement();
+    anchorPointY = sldStream.readElementText().toFloat();
 
-  //        GisLObject::_errorMessage;
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNextStartElement();
-  fillColor.setNamedColor(sldStream.readElementText());
+    //        GisLObject::_errorMessage;
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNextStartElement();
+    fillColor.setNamedColor(sldStream.readElementText());
 
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  sldStream.readNext();
-  maxDisplacement = sldStream.readElementText().toFloat();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    sldStream.readNext();
+    maxDisplacement = sldStream.readElementText().toFloat();
 }
 
 QColor TextSymbolizer::getDefColor(const std::string& s) {
-  switch (StringOperate::hash_(s.c_str())) {
-  case "fill"_hash:
-    return fillColor;
-  default:
-    return nullptr;
-  }
+    switch (StringOperate::hash_(s.c_str())) {
+    case "fill"_hash:
+        return fillColor;
+    default:
+        return nullptr;
+    }
 }
 
 float TextSymbolizer::getDefFloat(const std::string& s) {
-  switch (StringOperate::hash_(s.c_str())) {
-  case "fontSize"_hash:
-    return fontSize;
-  case "anchorPointX"_hash:
-    return anchorPointX;
-  case "anchorPointY"_hash:
-    return anchorPointY;
-  case "maxDisplacement"_hash:
-    return maxDisplacement;
-  default:
-    return 0;
-  }
+    switch (StringOperate::hash_(s.c_str())) {
+    case "fontSize"_hash:
+        return fontSize;
+    case "anchorPointX"_hash:
+        return anchorPointX;
+    case "anchorPointY"_hash:
+        return anchorPointY;
+    case "maxDisplacement"_hash:
+        return maxDisplacement;
+    default:
+        return 0;
+    }
 }
 
 std::string TextSymbolizer::getDefString(const std::string& s) {
-  switch (StringOperate::hash_(s.c_str())) {
-  case "propertyName"_hash:
-    return propertyName;
-  case "fontFamily"_hash:
-    return fontFamily;
-  default:
-    return "";
-  }
+    switch (StringOperate::hash_(s.c_str())) {
+    case "propertyName"_hash:
+        return propertyName;
+    case "fontFamily"_hash:
+        return fontFamily;
+    default:
+        return "";
+    }
 }
 } // namespace gisl
