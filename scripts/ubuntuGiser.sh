7e2c092045d42dd11aec85bb1ae5cca57edc6c81
knowmore <goooog@88.com>
2021-06-16 13:53:24 +0800

    fix: action doxygen

--- scripts/ubuntuGiser.sh
+++ scripts/ubuntuGiser.sh
@@ -1,4 +1,4 @@
-apt-get install ubuntu-desktop gcc cmake build-essential qtcreator qt5-default libgdal-dev gdal-bin python python3 python-pip python3-pip ccache git g++virtualenv curl python-setuptools build-essential gcc g++ cmake python3-dev binutils libproj-dev libgdal-dev python3-gdal gdal-bin doxygen-latex  doxygen-doc doxygen-gui graphviz qgisqttools5* qtpositioning5-dev gmt
+apt-get install ubuntu-desktop gcc cmake build-essential qtcreator qt5-default libgdal-dev gdal-bin python python3 python-pip python3-pip ccache git g++ virtualenv curl python-setuptools build-essential gcc g++ cmake python3-dev binutils libproj-dev libgdal-dev python3-gdal gdal-bin doxygen-latex doxygen-doc doxygen-gui graphviz qgisqttools5* qtpositioning5-dev gmt
 add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 apt-get update && apt-get upgrade -y
 #update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
