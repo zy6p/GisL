eebdc6c9ca0407f4fb87fa87d4ce5f7b4b0ba27a
knowmore <goooog@88.com>
2021-06-16 18:35:29 +0800

    feat: test add raster provider

--- tests/core/testdataprovider.cpp
+++ tests/core/testdataprovider.cpp
@@ -4,18 +4,26 @@
 
 #define CATCH_CONFIG_MAIN
 
-#include <absl/strings/string_view.h>
 #include <catch2/catch.hpp>
+#include <core/provider/rasterprovider.h>
+#include <core/provider/vectorprovider.h>
+#include <memory>
 #include <string>
 
-#include "core/provider/dataprovider.h"
-#include "core/provider/vectorprovider.h"
-
 TEST_CASE("VectorProvider::loadData") {
-  gisl::VectorProvider* pVectorProvider = new gisl::VectorProvider();
+  auto pVectorProvider =
+      std::make_unique<gisl::VectorProvider>(gisl::VectorProvider());
   std::string fileName = "../tests/data/qu.geojson";
   pVectorProvider->loadData(fileName);
 
   CHECK(pVectorProvider->getLayerCount() == 1);
-  delete pVectorProvider;
+}
+
+TEST_CASE("RasterProvider::loadData") {
+  auto pRasterProvider =
+      std::make_unique<gisl::RasterProvider>(gisl::RasterProvider());
+  std::string fileName = "../tests/data/fwi-200902.tif";
+  pRasterProvider->loadData(fileName);
+
+  CHECK(pRasterProvider->getLayerCount() == 1);
 }
