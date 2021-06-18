aaf5bfc3ddf6211469fec184da72984b959d5395
Restyled.io <commits@restyled.io>
2021-06-18 05:13:38 +0000

    Restyled by astyle

--- src/analysis/analysis.cpp
+++ src/analysis/analysis.cpp
@@ -8,32 +8,32 @@
 #include "rsindex.h"
 std::shared_ptr<gisl::Analysis> gisl::Analysis::_analysis = nullptr;
 std::shared_ptr<gisl::Analysis> gisl::Analysis::instance() {
-  if (nullptr == gisl::Analysis::_analysis) {
-    Analysis::_analysis = std::make_unique<Analysis>(Analysis{});
-  }
-  return Analysis::_analysis;
+    if (nullptr == gisl::Analysis::_analysis) {
+        Analysis::_analysis = std::make_unique<Analysis>(Analysis{});
+    }
+    return Analysis::_analysis;
 }
 std::shared_ptr<gisl::Analysis::AnalysisAlgNameMap>
 gisl::Analysis::registerAll() noexcept {
-  return pAlgNameMap;
+    return pAlgNameMap;
 }
 gisl::AnalysisAlg*
 gisl::Analysis::buildAlg(AnalysisAlgEnum m, QWidget* parent) const noexcept {
-  switch (m) {
-  case AnalysisAlgEnum::Test:
-    return nullptr;
-  case AnalysisAlgEnum::GeoReference: {
-    auto* p = new GeoReference();
-    p->execute(parent);
-    return p;
-  }
-  case AnalysisAlgEnum::RsIndex: {
-    auto* p = new RsIndex();
-    p->execute(parent);
-    return p;
-  }
-  default:
-    return nullptr;
-  }
+    switch (m) {
+    case AnalysisAlgEnum::Test:
+        return nullptr;
+    case AnalysisAlgEnum::GeoReference: {
+        auto* p = new GeoReference();
+        p->execute(parent);
+        return p;
+    }
+    case AnalysisAlgEnum::RsIndex: {
+        auto* p = new RsIndex();
+        p->execute(parent);
+        return p;
+    }
+    default:
+        return nullptr;
+    }
 }
 gisl::Analysis::Analysis() = default;
--- tests/analysis/testgeoreference.cpp
+++ tests/analysis/testgeoreference.cpp
@@ -9,21 +9,21 @@
 #include "gisl_config.h"
 
 TEST_CASE("GeoReference") {
-  gisl::LayerTree* layerTree = gisl::LayerTree::getLayerTree();
-  auto input = std::make_shared<gisl::RasterProvider>(gisl::RasterProvider());
-  std::string fileName1 = STRINGIFY(TEST_DATA_DIR) "rs/2/wucesource.tif";
-  input->loadData(fileName1);
-  layerTree->append(input->getFid(), input.get());
+    gisl::LayerTree* layerTree = gisl::LayerTree::getLayerTree();
+    auto input = std::make_shared<gisl::RasterProvider>(gisl::RasterProvider());
+    std::string fileName1 = STRINGIFY(TEST_DATA_DIR) "rs/2/wucesource.tif";
+    input->loadData(fileName1);
+    layerTree->append(input->getFid(), input.get());
 //  auto ref = new gisl::RasterProvider();
 //  std::string fileName2 = STRINGIFY(TEST_DATA_DIR) "rs/2/wucehelo.tif";
 //  input->loadData(fileName2);
 //  layerTree->append(ref->getFid(), ref);
-  std::string fileName3 = STRINGIFY(TEST_DATA_DIR) "rs/2/transpos.csv";
+    std::string fileName3 = STRINGIFY(TEST_DATA_DIR) "rs/2/transpos.csv";
 
 //  auto alg = gisl::GeoReference();
 //  alg.realAlg(input.get(), ref, fileName3);
 
-  CHECK(input->getLayerCount() == 3);
+    CHECK(input->getLayerCount() == 3);
 //  CHECK(alg.getTrans2D().trans.rows() == 6);
 //  CHECK(alg.getTrans2D().trans.cols() == 2);
 //  delete ref;
