21749082726a9ff3e7914632ca23f61c76b5b342
Restyled.io <commits@restyled.io>
2021-06-19 16:43:50 +0000

    Restyled by astyle

--- src/analysis/georeference.h
+++ src/analysis/georeference.h
@@ -18,54 +18,54 @@ class RasterProvider;
 
 class Trans2D {
 public:
-  Eigen::MatrixXf inPos;
-  Eigen::MatrixXf refPos;
-  Eigen::Matrix2f accuracyPos;
-  Eigen::MatrixXf i2rTrans;
-  Eigen::MatrixXf i2rAdjustA;
-  Eigen::MatrixXf r2iTrans;
-  Eigen::MatrixXf r2iAdjustA;
-  std::pair<int, int> transRecMN;
-  Eigen::Matrix<float, 4, 2> inRecPos;
-  Eigen::Matrix<float, 4, 2> outRecPos;
+    Eigen::MatrixXf inPos;
+    Eigen::MatrixXf refPos;
+    Eigen::Matrix2f accuracyPos;
+    Eigen::MatrixXf i2rTrans;
+    Eigen::MatrixXf i2rAdjustA;
+    Eigen::MatrixXf r2iTrans;
+    Eigen::MatrixXf r2iAdjustA;
+    std::pair<int, int> transRecMN;
+    Eigen::Matrix<float, 4, 2> inRecPos;
+    Eigen::Matrix<float, 4, 2> outRecPos;
 
-  void loadPosData(std::string_view sv);
-  void adjust();
-  [[nodiscard]] std::pair<float, float>
-  i2rTransPoint(const std::pair<int, int>& p) const noexcept;
-  [[nodiscard]] std::pair<float, float>
-  r2iTransPoint(const std::pair<int, int>& p) const noexcept;
-  void i2rTransRectangle(const gisl::Rectangle& in);
-  [[nodiscard]] bool isIInsideR(int x, int y) const noexcept;
-  [[nodiscard]] bool isRInsideI(const std::pair<int, int>& q) const noexcept;
+    void loadPosData(std::string_view sv);
+    void adjust();
+    [[nodiscard]] std::pair<float, float>
+    i2rTransPoint(const std::pair<int, int>& p) const noexcept;
+    [[nodiscard]] std::pair<float, float>
+    r2iTransPoint(const std::pair<int, int>& p) const noexcept;
+    void i2rTransRectangle(const gisl::Rectangle& in);
+    [[nodiscard]] bool isIInsideR(int x, int y) const noexcept;
+    [[nodiscard]] bool isRInsideI(const std::pair<int, int>& q) const noexcept;
 };
 
 class GeoReference final : public AnalysisAlg {
 public:
-  void execute(QWidget* parent) override;
-  ~GeoReference() override;
-  void initGui() override;
-  void reverse() override;
-  const std::string& output() override;
-  void execAlg() override;
+    void execute(QWidget* parent) override;
+    ~GeoReference() override;
+    void initGui() override;
+    void reverse() override;
+    const std::string& output() override;
+    void execAlg() override;
 
-  /**
-   * the real alg method, and can be called without gui
-   * @param input
-   * @param ref
-   * @param posFileName
-   */
-  void realAlg(const RasterProvider* input, std::string_view posFileName);
-  [[nodiscard]] const Trans2D& getTrans2D() const;
+    /**
+     * the real alg method, and can be called without gui
+     * @param input
+     * @param ref
+     * @param posFileName
+     */
+    void realAlg(const RasterProvider* input, std::string_view posFileName);
+    [[nodiscard]] const Trans2D& getTrans2D() const;
 
 private:
-  QComboBox* pProviderBox = nullptr;
-  QLineEdit* pPosLineEdit = nullptr;
+    QComboBox* pProviderBox = nullptr;
+    QLineEdit* pPosLineEdit = nullptr;
 
-  Eigen::MatrixXf out;
-  QImage outImage;
+    Eigen::MatrixXf out;
+    QImage outImage;
 
-  Trans2D trans2D;
+    Trans2D trans2D;
 };
 } // namespace gisl
 #endif // GISL_GEOREFERENCE_H
--- src/core/geometry/rectangle.cpp
+++ src/core/geometry/rectangle.cpp
@@ -7,45 +7,47 @@
 
 namespace gisl {
 Rectangle::Rectangle(OGREnvelope& envelope) : pmEnvelope(&envelope) {
-  minX = pmEnvelope->MinX;
-  maxX = pmEnvelope->MaxX;
-  minY = pmEnvelope->MinY;
-  maxY = pmEnvelope->MaxY;
+    minX = pmEnvelope->MinX;
+    maxX = pmEnvelope->MaxX;
+    minY = pmEnvelope->MinY;
+    maxY = pmEnvelope->MaxY;
 }
 
 void Rectangle::merge(const Rectangle& sOther) {
-  minX = MIN(minX, sOther.minX);
-  maxX = MAX(maxX, sOther.maxX);
-  minY = MIN(minY, sOther.minY);
-  maxY = MAX(maxY, sOther.maxY);
+    minX = MIN(minX, sOther.minX);
+    maxX = MAX(maxX, sOther.maxX);
+    minY = MIN(minY, sOther.minY);
+    maxY = MAX(maxY, sOther.maxY);
 }
 
 void Rectangle::merge(double dfX, double dfY) {
-  minX = MIN(minX, dfX);
-  maxX = MAX(maxX, dfX);
-  minY = MIN(minY, dfY);
-  maxY = MAX(maxY, dfY);
+    minX = MIN(minX, dfX);
+    maxX = MAX(maxX, dfX);
+    minY = MIN(minY, dfY);
+    maxY = MAX(maxY, dfY);
 }
 
 QPointF Rectangle::center() const {
-  return {maxX / 2 + minX / 2, maxY / 2 + minY / 2};
+    return {maxX / 2 + minX / 2, maxY / 2 + minY / 2};
 }
 
-QVector4D Rectangle::center4D() const { return QVector4D(center()); }
+QVector4D Rectangle::center4D() const {
+    return QVector4D(center());
+}
 
 QRectF Rectangle::getQRectF() const {
-  return {minX, maxY, maxX - minX, maxY - minY};
+    return {minX, maxY, maxX - minX, maxY - minY};
 }
 Rectangle::Rectangle(float minX, float maxX, float minY, float maxY) {
-  this->minX = minX;
-  this->minY = minY;
-  this->maxX = maxX;
-  this->maxY = maxY;
+    this->minX = minX;
+    this->minY = minY;
+    this->maxX = maxX;
+    this->maxY = maxY;
 }
 constexpr bool
 Rectangle::inside(const std::pair<float, float>& p) const noexcept {
-  return p.first < this->maxX && p.first > this->minX &&
-         p.second < this->maxY && p.second > this->minY;
+    return p.first < this->maxX && p.first > this->minX &&
+           p.second < this->maxY && p.second > this->minY;
 }
 Rectangle::Rectangle() = default;
 } // namespace gisl
