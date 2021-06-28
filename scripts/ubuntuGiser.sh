518e9a2d6a5bc2f1a30f8867025402c67b9c517f
knowmore <1270336479@qq.com>
2021-06-28 21:56:54 +0800

    fix: script format

--- scripts/ubuntuGiser.sh
+++ scripts/ubuntuGiser.sh
@@ -1,3 +1,35 @@
 add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 apt-get update && apt-get upgrade -y
-apt-get install ubuntu-desktop gcc cmake build-essential qtcreator qt5-default libgdal-dev gdal-bin python python3 python-pip python3-pip ccache git g++ virtualenv curl python-setuptools build-essential gcc g++ cmake python3-dev binutils libproj-dev libgdal-dev python3-gdal gdal-bin doxygen-latex doxygen-doc doxygen-gui graphviz qgisqttools5* qtpositioning5-dev gmt python3-breathe python3-sphinx libqwt-headers libqwt-qt5-6 libqwt-qt5-dev
+apt-get install ubuntu-desktop \
+  gcc \
+  cmake \
+  build-essential \
+  qtcreator \
+  qt5-default \
+  libgdal-dev \
+  gdal-bin \
+  python3 \
+  python3-pip \
+  ccache \
+  git \
+  g++ \
+  virtualenv \
+  curl \
+  python-setuptools \
+  python3-dev \
+  binutils \
+  libproj-dev \
+  libgdal-dev \
+  python3-gdal \
+  gdal-bin \
+  doxygen-latex \
+  doxygen-doc \
+  graphviz \
+  qgisqttools5* \
+  qtpositioning5-dev \
+  gmt \
+  python3-breathe \
+  python3-sphinx \
+  libqwt-headers \
+  libqwt-qt5-6 \
+  libqwt-qt5-dev
